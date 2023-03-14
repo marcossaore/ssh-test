b75d687af129eb9a84b9f794ac3f439d30e248ea
Marcos Soares <marcos.soares@modalservice.com>
2023-03-14 19:26:06 -0300

    change script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 echo ${{ secrets.PROPS }} > .env
-ssh -t ${{ secrets.HOST_NAME }} 'bash -i -c "mkdir -p app"'
+ssh -t ${{ secrets.HOST_NAME }} mkdir -p app
 scp -r . $HOST_NAME:/root/app
-ssh -t root@server -t 'bash -i -c "cd app && npm i && npm start"'
+ssh -t root@server -t 'cd app && npm i && npm start'
